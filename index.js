229ea4a815aeb9dbd75ded66b82ce31d39f28437
muslumhanerol <132482365+muslumhanerol@users.noreply.github.com>
2024-03-01 00:15:22 +0300

    plus

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
+import cart from "./cart.js";
 let app = document.getElementById('app');
 let temporaryContent = document.getElementById('temporaryContent');
 
 const loadTemplate = () => {
     fetch('/template.html')
     .then(response => response.text())
+    .then(html => {
+        app.innerHTML = html;
+        let contentTab = document.getElementById('contentTab');
+        contentTab.innerHTML = temporaryContent.innerHTML;
+        temporaryContent.innerHTML = null;
+        cart();
+
+    })
 }
 loadTemplate();
